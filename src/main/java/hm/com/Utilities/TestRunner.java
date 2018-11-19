eb903ebde9346095ad29088039bd2445b65d0a19
Saravanan Annatchi <44114962+SaravananAnnatchi@users.noreply.github.com>
2018-11-19 17:36:24 +0100

    Add files via upload

--- src/main/java/hm/com/Utilities/TestRunner.java
+++ src/main/java/hm/com/Utilities/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\NetEnt\\src\\test\\java\\Features\\defaultSlotMachine.feature",glue = "StepDefination"
+@CucumberOptions(features = "C:\\selenium-test\\Gaming\\NetEnt-master\\src\\test\\java\\Features\\defaultSlotMachine.feature",glue = "StepDefination"
                     ,monochrome = true, strict = true,format = {"pretty", "html:test-output"})
 public class TestRunner {
 }
